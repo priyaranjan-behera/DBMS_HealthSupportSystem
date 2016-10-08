7d382ce658e5d7009f61206ea0e6653f4ed0a22b
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-10-07 21:00:27 -0400

    Adding JDBC Integration:

--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/PeopleDao.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/PeopleDao.java
@@ -1,8 +1,39 @@
 package com.dbms.healthsupport.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class PeopleDao {
 	
-	Integer SSN;
-	String firstName;
-	String lastName;
+	static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
+	
+	static String user = "pbehera";	// For example, "jsmith"
+    static String passwd = "200106212";	// Your 9 digit student ID number
+	
+	
+	
+	public static void main(String[] args) throws SQLException
+	{
+		//Connection conn = DriverManager.getConnection(jdbcURL,user,passwd);
+		Connection conn = DriverManager.getConnection
+				  ("jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01", "pbehera", "200106212");
+		
+		Statement stmt = conn.createStatement();
+		
+		String createSQL = " CREATE TABLE table1 ("
+				+ "SSN Integer,"
+				+ "FirstName VARCHAR(10))";
+		
+		String insertSQL = "INSERT INTO table1 VALUES(12345, 'Akshay')";
+		
+		ResultSet rs = stmt.executeQuery(createSQL);
+		
+	}
+	
+	
+	
+
 }
---
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/People.java
@@ -0,0 +1,8 @@
+package com.dbms.healthsupport.domain;
+
+public class People {
+
+	Integer ssn;
+	String FirstName;
+	String LastName;
+}
