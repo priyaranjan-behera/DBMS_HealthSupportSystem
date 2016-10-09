8de0e48d06b4dce2e261b2cc4da687ea5ce76f0b
akshaynayak <akshaynayak2012@gmail.com>
2016-10-09 13:51:16 -0400

    Added observation spec

---
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/ObservationSpecDao.java
@@ -0,0 +1,20 @@
+package com.dbms.healthsupport.dao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
+public class ObservationSpecDao implements DaoInterface<ObservationSpecDao>{
+	public void createTable() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void insertData(){
+
+	}
+	
+}
