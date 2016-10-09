72fa3e94ecc96b64cec3a3dc6e604ddc2ec92f7a
Priyaranjan Behera <priyaranjan.nitr@gmail.com>
2016-10-09 12:56:04 -0400

    Test Changes

---
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/DaoInterface.java
@@ -0,0 +1,8 @@
+package com.dbms.healthsupport.dao;
+
+public interface DaoInterface<T> {
+	
+	void createTable();
+	void insertData();
+
+}
--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/PeopleDao.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/PeopleDao.java
@@ -6,43 +6,22 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class PeopleDao {
-	
-	static final String jdbcURL = "jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01";
-	
-	static String user = "pbehera";	// For example, "jsmith"
-    static String passwd = "200106212";	// Your 9 digit student ID number
-	
-	
-	
-	public static void main(String[] args) throws SQLException
-	{
-		//Connection conn = DriverManager.getConnection(jdbcURL,user,passwd);
-		Connection conn = DriverManager.getConnection
-				  ("jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01", "pbehera", "200106212");
-		
-		Statement stmt = conn.createStatement();
-		
-		//String createSQL = " CREATE TABLE table1 ("
-		//		+ "SSN Integer,"
-		//		+ "FirstName VARCHAR(10))";
-		
-		String insertSQL = "INSERT INTO table1 VALUES(12345, 'Akshay')";
-		ResultSet rs = stmt.executeQuery(insertSQL);
-		
-		String selectSQL = "SELECT SSN, FirstName FROM table1";
-		rs = stmt.executeQuery(selectSQL);
-		
-		while(rs.next())
-		{
-			Long r1 = rs.getLong("SSN");
-			System.out.println("Name: " + r1 );
-			
-			String r2 = rs.getString("FirstName");
-			System.out.println("Name: " + r2);
-		}
+public class PeopleDao implements DaoInterface<PeopleDao> {
+
+	public void createTable() {
+		// TODO Auto-generated method stub
 		
 	}
+
+	public void insertData() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+	
+	
 	
 	
 	
