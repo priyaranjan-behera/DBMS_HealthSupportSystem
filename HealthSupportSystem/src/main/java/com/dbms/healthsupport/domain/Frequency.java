56c5627f7ed2bea516ffa71692c04f1af28cf919
Priyaranjan Behera <pbehera@ncsu.edu>
2016-10-14 20:14:24 -0400

    Change to constructor.

--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/Frequency.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/Frequency.java
@@ -4,5 +4,24 @@ public class Frequency {
 	
 	String frequencyDesc;
 	Integer duration;
+	
+	
+	public Frequency(String frequencyDesc, Integer duration) {
+		super();
+		this.frequencyDesc = frequencyDesc;
+		this.duration = duration;
+	}
+	public String getFrequencyDesc() {
+		return frequencyDesc;
+	}
+	public void setFrequencyDesc(String frequencyDesc) {
+		this.frequencyDesc = frequencyDesc;
+	}
+	public Integer getDuration() {
+		return duration;
+	}
+	public void setDuration(Integer duration) {
+		this.duration = duration;
+	}
 
 }
