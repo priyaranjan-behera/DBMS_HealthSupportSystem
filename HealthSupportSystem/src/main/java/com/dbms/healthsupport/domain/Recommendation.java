6120ccdc63050568a25e3e6e2a52b24de3363eaf
samrudhisharma <ssharm17@.ncsu.edu>
2016-10-16 20:09:47 -0400

    Minor Fixes

--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/Recommendation.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/Recommendation.java
@@ -2,36 +2,51 @@ package com.dbms.healthsupport.domain;
 
 public class Recommendation {
 	
+	public Recommendation(Integer recId, String frequencyName, Integer threshold, Integer observationSpecification) {
+		super();
+		this.recId = recId;
+		this.frequencyName = frequencyName;
+		this.threshold = threshold;
+		this.observationSpecification = observationSpecification;
+	}
+
 	Integer recId;
 	String frequencyName;
 	Integer threshold;
 	
 	Integer observationSpecification;
-	
-	
-	public Integer getrecId() {
+
+	public Integer getRecId() {
 		return recId;
 	}
-	public void setrecId(Integer recId) {
+
+	public void setRecId(Integer recId) {
 		this.recId = recId;
 	}
-	public Integer getfreqId() {
-		return freqId;
+
+	public String getFrequencyName() {
+		return frequencyName;
 	}
-	public void setfreqId(Integer freqId) {
-		this.freqId = freqId;
+
+	public void setFrequencyName(String frequencyName) {
+		this.frequencyName = frequencyName;
 	}
+
 	public Integer getThreshold() {
 		return threshold;
 	}
+
 	public void setThreshold(Integer threshold) {
 		this.threshold = threshold;
 	}
-	public Recommendation(Integer recId, Integer freqId, Integer threshold) {
-		super();
-		this.recId = recId;
-		this.freqId = freqId;
-		this.threshold = threshold;
+
+	public Integer getObservationSpecification() {
+		return observationSpecification;
 	}
+
+	public void setObservationSpecification(Integer observationSpecification) {
+		this.observationSpecification = observationSpecification;
+	}
+	
 
 }
--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/WellPatient.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/domain/WellPatient.java
@@ -8,7 +8,7 @@ import com.dbms.healthsupport.dao.HealthSupporterDetails;
 public class WellPatient extends Patient {
 
 	public WellPatient(Long ssn, String firstName, String lastName, String address, Date dob, String gender,
-			HealthSupporterDetails primaryHealthSupporter, List<HealthSupporterDetails> secondaryHealthSupporters) {
+			String primaryHealthSupporter, List<String> secondaryHealthSupporters) {
 		super(ssn, firstName, lastName, address, dob, gender, primaryHealthSupporter, secondaryHealthSupporters);
 		// TODO Auto-generated constructor stub
 	}
