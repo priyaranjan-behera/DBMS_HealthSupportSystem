b710a7545588f63ce671d3cf5e2db56a32d130cf
vette <vivek.ette@gmail.com>
2016-10-21 01:12:18 -0400

    FrequencyDao

---
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/FrequencyDao.java
@@ -0,0 +1,62 @@
+package com.dbms.healthsupport.dao;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.dbms.healthsupport.domain.Frequency;
+import com.dbms.healthsupport.domain.Patient;
+import com.dbms.healthsupport.domain.People;
+
+
+public class FrequencyDao implements DaoInterface<Frequency> {
+	
+	Connection conn;
+	
+	public FrequencyDao() throws SQLException
+	{
+		this.conn = DriverManager.getConnection
+				  ("jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01", "pbehera", "200106212");
+		
+	}
+
+	public void insertRow(Frequency x) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void deleteRow(Frequency x) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public List<Frequency> getAllData() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Frequency getDataById(Object id) throws Exception {
+		// TODO Auto-generated method stub
+		
+		Statement stmt = conn.createStatement();
+		String selectSQL = "SELECT * FROM FREQUENCY";
+		
+		ResultSet rs = stmt.executeQuery(selectSQL);
+		
+		if(rs.next()){
+			String frequencyDesc = rs.getString("frequencyDesc");
+			Integer duration = rs.getInt("duration");
+			
+			return new Frequency(frequencyDesc, duration);
+			
+		}
+		return null;
+	}
+	
+
+}
