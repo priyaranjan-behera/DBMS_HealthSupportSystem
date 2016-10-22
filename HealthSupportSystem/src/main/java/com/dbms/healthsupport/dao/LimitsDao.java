2b95eae16da0a44909628d87d6d8eef99629fcca
vette <vivek.ette@gmail.com>
2016-10-21 22:49:52 -0400

    limits

--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/LimitsDao.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/dao/LimitsDao.java
@@ -8,6 +8,7 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.dbms.healthsupport.domain.Frequency;
 import com.dbms.healthsupport.domain.Limits;
 import com.dbms.healthsupport.domain.People;
 
@@ -22,77 +23,42 @@ public class LimitsDao implements DaoInterface<Limits>{
 				  ("jdbc:oracle:thin:@orca.csc.ncsu.edu:1521:orcl01", "pbehera", "200106212");
 		
 	}
-	public void createTable() throws Exception {
+
+	public void insertRow(Limits x) throws Exception {
 		// TODO Auto-generated method stub
-Statement stmt = conn.createStatement();
-	    
-		String createSQL = " CREATE TABLE LIMITS ("
-				+ "LimitID Integer,"
-				+ "LowerLimit Integer"
-				+ "UpperLimit Integer)";
-		
-		ResultSet rs = stmt.executeQuery(createSQL);
-		
-		
 		
 	}
 
-	public void insertData(Limits x) throws Exception {
+	public void deleteRow(Limits x) throws Exception {
 		// TODO Auto-generated method stub
-Statement stmt = conn.createStatement();
-	    
-		String insertSQL = " INSERT INTO LIMITS values ("
-				+ x.getlimitID() + "," 
-				+ x.getlowerLimit() + ","
-				+ x.getupperLimit() + ","
-				+ ")";
-		 
-		ResultSet rs = stmt.executeQuery(insertSQL);
-	}
-
-	public void deleteData(Limits x) throws Exception {
-		// TODO Auto-generated method stub
-		 Statement stmt = conn.createStatement();
-		    
-			String deleteSQL = " DELETE FROM LIMITS WHERE (limitID="
-					+ x.getlimitID()
-					+ ")";
-			 
-			ResultSet rs = stmt.executeQuery(deleteSQL);
 		
 	}
 
-	public List<Limits> getData() throws Exception {
+	public List<Limits> getAllData() throws Exception {
 		// TODO Auto-generated method stub
-Statement stmt = conn.createStatement();
-	    
-		String selectSQL = "SELECT * FROM LIMITS";
-		 
-		List<Limits> output = new ArrayList<Limits>();
-		
-		ResultSet rs = stmt.executeQuery(selectSQL);
-	
-
-		while(rs.next())
-		{
-			Integer limitID = rs.getInt("LimitID");
-			Integer lowerLimit = rs.getInt("LowerLimit");
-			Integer upperLimit = rs.getInt("UpperLimit");
-
-			
-			output.add(new Limits(limitID, lowerLimit, upperLimit));
-		}
-		
-		return output;
+		return null;
 	}
 
-	public void dropTable() throws Exception {
+	public Limits getDataById(Object id) throws Exception {
 		// TODO Auto-generated method stub
 		Statement stmt = conn.createStatement();
 	    
-		String dropSQL = "DROP TABLE LIMITS";
-		 
-		ResultSet rs = stmt.executeQuery(dropSQL);
+		String selectSQL = "SELECT * FROM LIMITS";
+		
+		ResultSet rs = stmt.executeQuery(selectSQL);
+		
+		while(rs.next()){
+			Integer limitID = rs.getInt("limitID");
+			Integer lowerlimit = rs.getInt("lowerlimit");
+			Integer upperlimit = rs.getInt("upperlimit");
+			String metricID = rs.getString("metricID");
+			String observationSpec = rs.getString("observationSpec");
+			
+			return new Limits(upperlimit, upperlimit, upperlimit, observationSpec, observationSpec);
+			
+		}
+		
+		return null;
 	}
 
 	
