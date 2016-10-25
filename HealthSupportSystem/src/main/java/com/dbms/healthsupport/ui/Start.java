8b8ed758527a995b1f8c2adf40f396f3f366c8fb
samrudhisharma <ssharm17@.ncsu.edu>
2016-10-25 19:16:52 -0400

    UI initial commit

---
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/ui/Start.java
@@ -0,0 +1,66 @@
+package com.dbms.healthsupport.ui;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+public class Start extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Start frame = new Start();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Start() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("Login");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnNewButton.setBounds(189, 101, 117, 29);
+		contentPane.add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Sign Up");
+		btnNewButton_1.setBounds(189, 166, 117, 29);
+		contentPane.add(btnNewButton_1);
+		
+		JButton btnNewButton_2 = new JButton("Exit System");
+		btnNewButton_2.setBounds(189, 224, 117, 29);
+		contentPane.add(btnNewButton_2);
+		
+		JLabel lblHealthSystemDbms = new JLabel("Health System DBMS");
+		lblHealthSystemDbms.setBounds(180, 54, 191, 16);
+		contentPane.add(lblHealthSystemDbms);
+	}
+
+}
