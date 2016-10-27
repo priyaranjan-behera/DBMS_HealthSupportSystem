591fe087f04ea36c54f39f931d7ae47e8a16424f
samrudhisharma <ssharm17@.ncsu.edu>
2016-10-27 01:31:08 -0400

    Health Indicators

--- HealthSupportSystem/src/main/java/com/dbms/healthsupport/ui/PatientLoggedIn.java
+++ HealthSupportSystem/src/main/java/com/dbms/healthsupport/ui/PatientLoggedIn.java
@@ -2,6 +2,8 @@ package com.dbms.healthsupport.ui;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -40,26 +42,60 @@ public class PatientLoggedIn extends JFrame {
 		contentPane.setLayout(null);
 		
 		JButton btnProfile = new JButton("Profile");
+		btnProfile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PatientProfile patientProfile = new PatientProfile();
+				patientProfile.setVisible(true);
+			}
+		});
 		btnProfile.setBounds(25, 38, 189, 29);
 		contentPane.add(btnProfile);
 		
 		JButton btnDiagnoses = new JButton("Diagnoses");
+		btnDiagnoses.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PatientProfile patientProfile = new PatientProfile();
+				patientProfile.setVisible(true);
+			}
+		});
 		btnDiagnoses.setBounds(25, 91, 195, 29);
 		contentPane.add(btnDiagnoses);
 		
 		JButton btnAlerts = new JButton("Alerts");
+		btnAlerts.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PatientAlerts patientAlert = new PatientAlerts();
+				patientAlert.setVisible(true);
+			}
+		});
 		btnAlerts.setBounds(25, 146, 189, 29);
 		contentPane.add(btnAlerts);
 		
 		JButton btnHealthIndicators = new JButton("Health Indicators");
+		btnHealthIndicators.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				//TO DO
+			}
+		});
 		btnHealthIndicators.setBounds(227, 91, 195, 29);
 		contentPane.add(btnHealthIndicators);
 		
 		JButton btnViewHealthSupporters = new JButton("View Health Supporters");
+		btnViewHealthSupporters.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				PatientViewMyHealthSupporters patientViewMyHealthSupporters = new PatientViewMyHealthSupporters();
+				patientViewMyHealthSupporters.setVisible(true);
+			}
+		});
 		btnViewHealthSupporters.setBounds(227, 38, 195, 29);
 		contentPane.add(btnViewHealthSupporters);
 		
 		JButton btnExitSystem = new JButton("Exit System");
+		btnExitSystem.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				System.exit(0);
+			}
+		});
 		btnExitSystem.setBounds(227, 146, 195, 29);
 		contentPane.add(btnExitSystem);
 	}
